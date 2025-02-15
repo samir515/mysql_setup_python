38ca7baf8f98184221f8a1ec1d1f5870f846b3ca
samir ghimire <sameerghimire515@gmail.com>
2025-02-15 22:19:49 +0545

    correcting validation

--- src/user_service.py
+++ src/user_service.py
@@ -1,32 +1,52 @@
 from database import get_db_connection
-# for creating user using get_db_connection
-def create_user(name,email,password,age,phone):
+
+# Function to check if email or phone exists
+def user_exists(email, phone):
     conn = get_db_connection()
-    cursor =conn.cursor()
-    query="insert into users(name,email,password,age,phone,is_active) values (%s,%s,%s,%s,%s,%s)"
-    cursor.execute(query,(name,email,password,age,phone,True))
+    cursor = conn.cursor()
+
+    query = "SELECT COUNT(*) FROM users WHERE email = %s OR phone = %s"
+    cursor.execute(query, (email, phone))
+    count = cursor.fetchone()[0]
+
+    cursor.close()
+    conn.close()
+    # Returns True if email/phone already exists
+    return count > 0
+
+
+# Create a new user with validation
+def create_user(name, email, password, age, phone):
+    if user_exists(email, phone):
+        return {"error": "Email or phone already exists. Please use a different one."}
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    query = "INSERT INTO users (name, email, password, age, phone, is_active) VALUES (%s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (name, email, password, age, phone, True))
     conn.commit()
     cursor.close()
     conn.close()
-    return {"message":"User created successfully"}
+    return {"message": "User created successfully"}
 
-# for getting all user using get_db_connection
-def get_users():
+# Fetch users based on active status
+def get_users(active_only=True):
     conn = get_db_connection()
-    cursor =conn.cursor(dictionary=True)
-    cursor.execute("select * from users")
-    users=cursor.fetchall()
-    conn.commit()
+    cursor = conn.cursor(dictionary=True)
+    if active_only:
+        cursor.execute("SELECT * FROM users WHERE is_active = TRUE")  # Fetch only active users
+    else:
+        cursor.execute("SELECT * FROM users")  # Fetch all users (active & inactive)
+    users = cursor.fetchall()
     cursor.close()
     conn.close()
     return users
 
 # for updating user using his id
-def update_user(user_id, name=None, email=None,age=None,phone=None):
+def update_user(user_id, name=None, email=None,age=None):
     conn = get_db_connection()
     cursor = conn.cursor()
-    query = "UPDATE users SET name = %s, email = %s,age = %s, phone = %s WHERE id = %s"
-    cursor.execute(query, (name, email,age,phone, user_id))
+    query = "UPDATE users SET name = %s, email = %s,age = %sWHERE id = %s"
+    cursor.execute(query, (name, email,age,user_id))
     conn.commit()
     cursor.close()
     conn.close()
